be70119e688928a5d55b927037a38bebdf9774d9
philip <adegokephilip001@gamil.com>
2024-10-30 21:28:08 +0300

    routes

--- part4/controllers/users.js
+++ part4/controllers/users.js
@@ -1,6 +1,25 @@
 const bcrypt = require('bcryptjs')
 const usersRouter = require('express').Router()
 const User = require('../models/user')
-const { response } = require('../app')
+const { response, request } = require('../app')
 
-usersRouter.post('/', async(request, response))
+usersRouter.post('/', async(request, response) => {
+    const {username, name, password } = request.body
+
+    const saltRounds = 10
+    const PasswordHash = await bcrypt.hash(password, saltRounds)
+
+    const user = new User ({
+        username,
+        name,
+        PasswordHash,
+    })
+    const savedUser = await user.save()
+    response.status(201).json(savedUser)
+})
+
+usersRouter.get('/', async(request, response) =>{
+    const user = await User.find({})
+    response.json(user)
+})
+module.exports = usersRouter
